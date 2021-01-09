02d2eef4f9912e7fb6e3329ea286c1ffc2e05c74
Vin <vineetgaur97@gmail.com>
2021-01-09 16:49:59 +0530

    First Commit

---
+++ Passport Visa Management System/Controllers/SignInController.cs
@@ -0,0 +1,33 @@
+﻿using Passport_Visa_Management_System.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Passport_Visa_Management_System.Controllers
+{
+    public class SignInController : Controller
+    {
+        // GET: SignIn
+        public ActionResult Index()
+        {
+            PassportVisaManagementSystemService.User U = new PassportVisaManagementSystemService.User();
+            return View(U);
+        }
+        [HttpPost]
+        public ActionResult Index(PassportVisaManagementSystemService.User U)
+        {
+            bool SignInResult = DbOperation.UserSignIn(U);
+            if (SignInResult)
+            {
+                return Redirect("/UserHome");
+            }
+            else
+            {
+
+                return View();
+            }
+        }
+    }
+}
---
+++ Passport Visa Management System/Models/DbOperation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Passport_Visa_Management_System.Models
+{
+    public class DbOperation
+    {
+        public static bool UserSignIn(PassportVisaManagementSystemService.User U)
+        {
+            PassportVisaManagementSystemService.Service1Client PVMS = new PassportVisaManagementSystemService.Service1Client();
+            return  PVMS.SignIn(U);
+        }
+    }
+}
