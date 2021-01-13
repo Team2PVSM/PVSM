ec176253d6185b9025b4b108403683ad97f44114
unknown <dharmik3504>
2021-01-13 22:15:05 +0530

    up

--- Passport Visa Management System/Models/DbOperation.cs
+++ Passport Visa Management System/Models/DbOperation.cs
@@ -7,10 +7,16 @@ namespace Passport_Visa_Management_System.Models
 {
     public class DbOperation
     {
+        public static bool UserSignUp(PassportVisaManagementSystemService.User U)
+        {
+            PassportVisaManagementSystemService.Service1Client PVMS = new PassportVisaManagementSystemService.Service1Client();
+            return PVMS.SignUp(U);
+
+        }
         public static bool UserSignIn(PassportVisaManagementSystemService.User U)
         {
             PassportVisaManagementSystemService.Service1Client PVMS = new PassportVisaManagementSystemService.Service1Client();
-            return  PVMS.SignIn(U);
+            return PVMS.SignIn(U.UserId,U.Password);
         }
     }
 }
