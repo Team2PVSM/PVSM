23b3590b2e5fa99e333edc744833969f9cbb9310
unknown <dharmik3504>
2021-01-13 22:03:41 +0530

    up

--- Passport Visa Management System/Controllers/SignInController.cs
+++ Passport Visa Management System/Controllers/SignInController.cs
@@ -13,21 +13,45 @@ namespace Passport_Visa_Management_System.Controllers
         public ActionResult Index()
         {
             PassportVisaManagementSystemService.User U = new PassportVisaManagementSystemService.User();
+            PassportVisaManagementSystemService.Service1Client PVMS = new PassportVisaManagementSystemService.Service1Client();
+            PassportVisaManagementSystemService.HintQuestion[] D  = PVMS.FetchHintQuestion();
+            ViewBag.HintQuestionDD = D.ToList();
             return View(U);
         }
         [HttpPost]
-        public ActionResult Index(PassportVisaManagementSystemService.User U)
+        public ActionResult Index(string AccountType, PassportVisaManagementSystemService.User U)
         {
-            bool SignInResult = DbOperation.UserSignIn(U);
-            if (SignInResult)
+            if (AccountType.ToLower()== "signup")
             {
-                return Redirect("/UserHome");
+                Session["SignUpMsg"] = null;
+                string str = "Dear User \n Your User Id is" +U.UserId+ "and your password is"+U.Password+ ".\nYou are planning for"+ U.ApplyType +"and your citizen type is"+U.CitizenType;
+                bool SignUpResult = DbOperation.UserSignUp(U);
+                if (SignUpResult)
+                {
+                    Session["SignUpMsg"]  = str;
+                    return Redirect("/UserHome");
+                }
+                else
+                {
+
+                    return View();
+                }
+
             }
             else
             {
+                bool SignInResult = DbOperation.UserSignIn(U);
+                if (SignInResult)
+                {
+                    return Redirect("/UserHome");
+                }
+                else
+                {
 
-                return View();
+                    return View();
+                }
             }
+
         }
     }
 }
