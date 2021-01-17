dfd231fd6cd02e3a3e3aad4b754a41977351dd24
unknown <dharmik3504>
2021-01-17 23:07:45 +0530

    17 jan

--- Passport Visa Management System/Controllers/SignInController.cs
+++ Passport Visa Management System/Controllers/SignInController.cs
@@ -13,30 +13,43 @@ namespace Passport_Visa_Management_System.Controllers
         // GET: SignIn
         public ActionResult Index()
         {
+            Session["Page"] = null;
             PassportVisaManagementSystemService.User U = new PassportVisaManagementSystemService.User();
             PassportVisaManagementSystemService.Service1Client PVMS = new PassportVisaManagementSystemService.Service1Client();
-            PassportVisaManagementSystemService.HintQuestion[] D  = PVMS.FetchHintQuestion();
+            PassportVisaManagementSystemService.HintQuestion[] D = PVMS.FetchHintQuestion();
             ViewBag.HintQuestionDD = D.ToList();
             return View(U);
         }
         [HttpPost]
         public ActionResult Index(string AccountType, PassportVisaManagementSystemService.User U)
         {
-                if (AccountType.ToLower()== "signup")
+           
+            if (AccountType.ToLower() == "signup")
             {
                 Session["SignUpMsg"] = null;
                 Session["userName"] = null;
                 Session["password"] = null;
                 string email = U.EmailAddress;
                 bool SignUpResult = DbOperation.UserSignUp(U);
-                var list = DbOperation.FetchUserByEmail(U.EmailAddress);
-                Session["userName"] = list[0].UserId;
-                Session["password"] = list[0].Password;
+                var list = DbOperation.FetchUserByEmail(U.EmailAddress); //email id  must be unique
+                if (list != null && list.Count == 1)
+                {
+                    Session["userName"] = list[0].UserId;
+                    Session["password"] = list[0].Password;
+                }
+                else
+                {
+                    int l = list.Count;
+                    Session["userName"] = list[l - 1].UserId;
+                    Session["password"] = list[l - 1].Password;
 
-                string str = "Dear User \n Your User Id is " + list[0].UserId+ " and your password is "+ list[0].Password+ ".\nYou are planning for "+ list[0].ApplyType +" and your citizen type is "+ list[0].CitizenType;
+                }
+
+                string str = "Dear User \n Your User Id is " + list[0].UserId + " and your password is " + list[0].Password + ".\nYou are planning for " + list[0].ApplyType + " and your citizen type is " + list[0].CitizenType;
                 if (SignUpResult)
                 {
-                    Session["SignUpMsg"]  = str;
+                    Session["Page"] = AccountType;
+                    Session["SignUpMsg"] = str;
                     return Redirect("/UserHome");
                 }
                 else
@@ -51,7 +64,8 @@ namespace Passport_Visa_Management_System.Controllers
                 bool SignInResult = DbOperation.UserSignIn(U);
                 if (SignInResult)
                 {
-            var userList = DbOperation.FetchUserByuserparameter("userid", U.UserId);
+                    Session["Page"] = AccountType;
+                    var userList = DbOperation.FetchUserByuserparameter("userid", U.UserId);
 
                     if (userList[0].ApplyType.ToLower() == "passport")
                     {
