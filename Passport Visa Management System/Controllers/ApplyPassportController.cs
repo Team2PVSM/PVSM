142482d34fad014e8a9a2bfffd779f95ff160c03
unknown <dharmik3504>
2021-01-19 17:48:27 +0530

    19 jan

--- Passport Visa Management System/Controllers/ApplyPassportController.cs
+++ Passport Visa Management System/Controllers/ApplyPassportController.cs
@@ -22,17 +22,22 @@ namespace Passport_Visa_Management_System.Controllers
         [HttpPost]
         public ActionResult Index(ApplyPassport A)
         {
-            var username=Request.Cookies["UserName"].Value.ToString();
+            
+            var username = Request.Cookies["UserName"].Value.ToString();
             ViewBag.CountryDD = null;
             Service1Client PVMS = new Service1Client();
             Country[] D = PVMS.FetchCountries();
             ViewBag.CountryDD = D.ToList();
+            if (checkForApplyPassportValidation(A))
+            {
+                return View();
+            }
             int userId = DbOperation.FetchIdByUserName(username);
             A.UserId = userId;
             bool successful = DbOperation.ApplyPassportNew(A);
             if (successful)
             {
-               var json= DbOperation.fetchApplyPassportbyUserId(userId);
+                var json = DbOperation.fetchApplyPassportbyUserId(userId);
                 Session["PassportNumber"] = json[0].PassportNumber;
                 Session["Amount"] = json[0].Amount;
                 Session["successMsg"] = "<b>Need the passport number while giving payment? Please note down your Id \n </b> \n" + json[0].PassportNumber + ".Passport application cost is Rs." + json[0].Amount;
@@ -41,20 +46,57 @@ namespace Passport_Visa_Management_System.Controllers
             }
             else
             {
-            
+
                 return View();
 
             }
         }
         public string GetStateByCountryId(int selectedCountry)
         {
-            ViewBag.stateListByCountry= DbOperation.FetchStateByCountryId(selectedCountry);
+            ViewBag.stateListByCountry = DbOperation.FetchStateByCountryId(selectedCountry);
             return ViewBag.stateListByCountry;
         }
         public string GetCityByStateId(int selectState)
         {
             ViewBag.cityListByState = DbOperation.FetchCityByStateId(selectState);
             return ViewBag.cityListByState;
+        }
+        public bool checkForApplyPassportValidation(ApplyPassport U)
+        {
+            //if (U.UserId ==0  || U.UserId.ToString().Trim().Length == 0)
+            //{
+            //    ModelState.AddModelError("UserId", "User Id cannot be empty");
+            //    return true;
+            //}
+             if (U.CountryId ==0  || U.CountryId.ToString().Trim().Length == 0)
+            {
+                ModelState.AddModelError("CountryId", "Country Id cannot be empty");
+                return true;
+            }
+            else if (U.StateId == 0 || U.StateId.ToString().Trim().Length == 0)
+            {
+                ModelState.AddModelError("StateId", "State Id cannot be empty");
+                return true;
+            }
+            else if (U.CityId == 0 || U.CityId.ToString().Trim().Length == 0)
+            {
+                ModelState.AddModelError("CityId", "City  cannot be empty");
+                return true;
+            }
+            else if (U.Pin == 0 || U.Pin.ToString().Trim().Length == 0)
+            {
+                ModelState.AddModelError("Pin", "Pin cannot be empty");
+                return true;
+            }
+            else if (U.IssueDate == null || U.IssueDate.ToString().Trim().Length == 0)
+            {
+                ModelState.AddModelError("IssueDate", "Issue Date cannot be empty");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
---
+++ Passport Visa Management System/Controllers/VisaCancellationController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Passport_Visa_Management_System.PassportVisaManagementSystemService;
+
+namespace Passport_Visa_Management_System.Controllers
+{
+    public class VisaCancellationController : Controller
+    {
+        // GET: VisaCancellation
+        public ActionResult Index()
+        {
+            ApplyVisa AV = new ApplyVisa();
+
+            return View(AV);
+        }
+        [HttpPost]
+        public ActionResult Index(ApplyVisa AV)
+        {
+
+            return View();
+        }
+        }
+}
