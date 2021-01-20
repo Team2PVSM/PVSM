7cf8120b1bc4e73030f58f5d5914df0759d12010
unknown <dharmik3504>
2021-01-20 16:38:06 +0530

    20 jan

--- Passport Visa Management System/Controllers/AuthenticationController.cs
+++ Passport Visa Management System/Controllers/AuthenticationController.cs
@@ -15,15 +15,30 @@ namespace Passport_Visa_Management_System.Controllers
         {
             User U = new User();
             Service1Client PVMS = new Service1Client();
-            HintQuestion[] D = PVMS.FetchHintQuestion();
-            ViewBag.HintQuestionDD = D.ToList();
+            //HintQuestion[] D = PVMS.FetchHintQuestion();
+            //ViewBag.HintQuestionDD = D.ToList();
+            var username = Request.Cookies["UserName"].Value.ToString();
+            TempData["HintQuestion"] = DbOperation.FetchHintQuestionByUserName(username);
             return View(U);
         }
         [HttpPost]
         public ActionResult Index(User U)
         {
-            DbOperation.Authenticity(U);
-            return View();
+            var username = Request.Cookies["UserName"].Value.ToString();
+            int userId = DbOperation.FetchIdByUserName(username);
+            TempData["HintQuestion"] = DbOperation.FetchHintQuestionByUserName(username);
+            U.UserId = username;
+            
+            if (DbOperation.Authenticity(U))
+            {
+                return Redirect("/VisaCancellation");
+            }
+            else
+            {
+                TempData["Msg"] = "Answer does not Match.Please Try Again";
+                return View();
+
+            }
         }
     }
 }
